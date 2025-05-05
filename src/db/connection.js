187c1c2b9b645e96dcd2e85ed88085ee6ef23e2d
utkarsh-jschamps <utkarsh.jschamps@gmail.com>
2025-05-05 10:20:55 +0530

    database details moved into .env

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Admin@123',
-  database: 'manage_admission'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
 });
 
 connection.connect((err) => {
